a2b9daf226bd7f310d61faf46835455296b4d8fb
Junzhuo Li <junzhuo@cs.ualberta.ca>
2017-03-22 20:37:52 -0600

    add a lot

--- app/src/main/java/yifanwang/mymood1/CameroActivity.java
+++ app/src/main/java/yifanwang/mymood1/CameroActivity.java
@@ -1,8 +1,12 @@
 package yifanwang.mymood1;
 
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import android.app.Activity;
 import android.content.Intent;
@@ -22,15 +26,22 @@ import android.widget.TextView;
 public class CameroActivity extends AppCompatActivity {
 
     Uri imageFileUri;
+    String mCurrentPhotoPath;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_camero);
         ImageButton ib = (ImageButton) findViewById(R.id.takeImage_ib);
         ib.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
-                takeAPhoto();
+                try{
+                    takeAPhoto();
+                } catch (IOException e){
+                    e.printStackTrace();
+                }
+
             }
         });
     }
@@ -53,14 +64,30 @@ public class CameroActivity extends AppCompatActivity {
         }
         return super.onOptionsItemSelected(item);
     }
-    public void takeAPhoto(){
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = new File(Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_DCIM), "Camera");
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        mCurrentPhotoPath = "file:" + image.getAbsolutePath();
+        return image;
+    }
+    public void takeAPhoto() throws IOException {
         String path = Environment.getExternalStorageDirectory().getAbsolutePath() + "/MyCameraTest";
         File folder = new File(path);
         if (!folder.exists())
             folder.mkdir();
         String imagePathAndFileName = path + File.separator + String.valueOf(System.currentTimeMillis()) + ".jpg";
         File imageFile = new File(imagePathAndFileName);
-        imageFileUri = Uri.fromFile(imageFile);
+        imageFileUri = FileProvider.getUriForFile(this, this.getApplicationContext().getPackageName() + ".provider", createImageFile());
 
         Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         intent.putExtra(MediaStore.EXTRA_OUTPUT, imageFileUri);
